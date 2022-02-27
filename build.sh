5aab8b71753271bc6bc81932c12192327b6a0150
Caleb Webber <caleb@codingthemsoftly.com>
2022-02-27 12:36:48 -0500

    chore: clean up build script

--- build.sh
+++ build.sh
@@ -1,2 +1,11 @@
-rm build/table_export.zip
-zip -r build/table_export.zip * -x .gitignore build/* test/* build.sh README.md
+BUILD_PATH=out
+
+if [ ! -d $BUILD_PATH ]; then
+    mkdir $BUILD_PATH
+fi
+
+if [ -f $BUILD_PATH/table_export.zip ]; then
+    rm $BUILD_PATH/table_export.zip
+fi
+
+zip -r $BUILD_PATH/table_export.zip * -x .gitignore "$BUILD_PATH/*" "test/*" build.sh README.md
