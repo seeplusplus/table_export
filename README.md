039a15d63ca69cd2314173e103c074c8365849ef
Caleb Webber <caleb@codingthemsoftly.com>
2022-02-27 11:20:05 -0500

    feat: complete first rc of 1.0.0

--- README.md
+++ README.md
@@ -8,7 +8,14 @@ Notifies you when there's a table on the page. Provides download for tables as a
 | File | Work |
 -------|------
 |tables.js| Content script for detecting tables in page and communicating page info to the service worker.|
-|backgroun.js| Receives messages from content script|
+|background.js| Receives messages from content script|
+
+## TODO
+
+1. Add notifier on extension icon to notify user that tables have been found on page w/ count.
+2. Save as dialog
+3. Disable Download as CSV when user right clicks on something that isn't a table
+4. Handle tfoot, thead, and colspan
 
 # Attribution
 
--- background.js
+++ background.js
@@ -1,16 +1,71 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.type === "table_found") {
-        table_count = parseInt(message.payload.tables_count, 10);
-        if (isNaN(table_count)) {
+const downloadTableContextMenuId = '100';
+
+var backgroundState = {
+    tableCsv: null,
+    tableUrl: null,
+};
+
+function enableContextMenu() {
+    chrome.contextMenus.update(
+        downloadTableContextMenuId,
+        {
+            enabled: true,
+            visible: true
+        }
+    );
+}
+
+function setupContextMenu() {
+    const contextMenuProperties = {
+        id: downloadTableContextMenuId,
+        contexts: [
+            'page',
+        ],
+        enabled: false, // TODO: disabled when user doesn't right click on table.
+        title: 'Download table as CSV',
+        // visible: false,
+    }
+    chrome.contextMenus.create(contextMenuProperties);
+}
+
+function setupEventListeners(state) {
+
+    chrome.runtime.onInstalled.addListener(setupContextMenu);
+
+    chrome.contextMenus.onClicked.addListener(async (info, tab) => {
+        if (!state.tableCsv) {
             return;
         }
-        chrome.action.setBadgeText({
-            text: `${table_count}`
+
+        const downloadId = await chrome.downloads.download({
+            url: state.tableUrl,
+           // saveAs: true, // TODO: This never opens nor does it start a download when true.
         });
-        chrome.action.setBadgeBackgroundColor({
-            color: '#00FF00'
-        });
-    } else if (message.type === "table_clicked") {
-        console.log(message.payload.as_csv);
-    }
-});
+    });
+    
+    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+        if (message.type === "table_found") {
+            table_count = parseInt(message.payload.tables_count, 10);
+            if (isNaN(table_count)) {
+                return;
+            }
+            chrome.action.setBadgeText({
+                text: `${table_count}`
+            });
+            chrome.action.setBadgeBackgroundColor({
+                color: '#00FF00'
+            });
+        } else if (message.type === "table_clicked") {
+            enableContextMenu();
+            state.tableCsv = message.payload.as_csv;
+            state.tableUrl = message.payload.payload_url;
+        }
+    });    
+}
+
+/**
+ * Event listeners
+ */
+
+
+setupEventListeners(backgroundState);
