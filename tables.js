9fee4b28719cec23294b285780db19fabb43fa8d
Caleb Webber <caleb@codingthemsoftly.com>
2022-02-27 11:20:05 -0500

    feat: complete first rc of 1.0.0

--- tables.js
+++ tables.js
@@ -8,32 +8,38 @@ function getCsvFromTableElement(table) {
 
 	return rows.reduce(
 	    (p, row) => {
-            let cols = [...row.querySelectorAll('td')];
+            let cols = [...row.querySelectorAll('td, th')];
             return [...p, [...cols].reduce((s, col) => [...s, col.textContent], []).join(', ')];
         },
 	    []
 	).join('\n');
 }
 
+function createFileFromString(string_data, name) {
+	return new File(string_data.split(), name, { type: 'text/csv' });
+}
+
 function bootstrapTableDetection() {
     let tables = document.querySelectorAll('table');
 
     // message plugin that tables are detected
-    if (tables.length > 0) {
-        chrome.runtime.sendMessage({ 
-            type: 'table_found',
-            payload: {
-                tables_count: tables.length}
-        });
-    }
+    // if (tables.length > 0) {
+    //     chrome.runtime.sendMessage({ 
+    //         type: 'table_found',
+    //         payload: {
+    //             tables_count: tables.length}
+    //     });
+    // }
 
     // hookup click listener for tables on page
     tables.forEach(t =>
-        t.addEventListener('click', function(event) {
+        t.addEventListener('contextmenu', function(event) {
+            const tableCsv = getCsvFromTableElement(event.currentTarget);
             chrome.runtime.sendMessage({
                 type: 'table_clicked',
                 payload: {
-                    as_csv: getCsvFromTableElement(event.currentTarget)
+                    as_csv: tableCsv,
+                    payload_url: URL.createObjectURL(createFileFromString(tableCsv))
                 }
             })
         })
